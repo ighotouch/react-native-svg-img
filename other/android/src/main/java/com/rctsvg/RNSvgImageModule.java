1617ff26b44518aa28a52add5f24049a3e33639d
Matthew Igho <ighotouch@gmail.com>
2018-07-21 17:29:34 +0100

    merged in babel feature

---
+++ other/android/src/main/java/com/rctsvg/RNSvgImageModule.java
@@ -0,0 +1,22 @@
+
+package com.rctsvg;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class RNSvgImageModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNSvgImageModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNSvgImage";
+  }
+}
